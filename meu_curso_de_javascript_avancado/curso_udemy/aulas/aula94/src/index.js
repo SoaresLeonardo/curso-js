f730bcd68a56b5cd92729b4baf0e0006c8dffc34
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-11-26 10:49:45 -0300

    modelo-webpack

---
+++ meu_curso_de_javascript_avancado/curso_udemy/aulas/aula94/src/index.js
@@ -0,0 +1,7 @@
+import { nome as nome2, sobrenome, idade, soma } from './modulo1';
+
+const nome = 'Lucas'
+
+console.log(nome, sobrenome, idade, nome2)
+
+console.log(soma(5, 5))
---
+++ meu_curso_de_javascript_avancado/curso_udemy/aulas/aula94/src/modulo1.js
@@ -0,0 +1,11 @@
+export const nome = 'Leonardo';
+export const sobrenome = 'Henrique';
+export const idade = 15;
+
+export function soma(x, y) {
+    return x + y;
+}
+
+
+
+// export { nome, sobrenome, idade, soma }
