096c63fe7bf572e55d3530e6e1c40689b6fd2b64
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-11-27 11:33:48 -0300

    aulas

---
+++ meu_curso_de_javascript_avancado/curso_udemy/aulas/node/aula1/app.js
@@ -0,0 +1,9 @@
+// const mod1 = require('./mod1');
+// const {nome, sobrenome, falaNome} = require('./mod1');
+
+// console.log(falaNome())
+
+const path = require('path');
+const axios = require('axios')
+const { Pessoa } = require('./mod1')
+
