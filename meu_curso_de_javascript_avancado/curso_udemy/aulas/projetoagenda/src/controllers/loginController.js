ecab1833767120cfa25033278576e19a96cdf44a
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-03 21:31:54 -0300

    att

--- meu_curso_de_javascript_avancado/curso_udemy/aulas/projetoagenda/src/controllers/loginController.js
+++ meu_curso_de_javascript_avancado/curso_udemy/aulas/projetoagenda/src/controllers/loginController.js
@@ -1,20 +1,61 @@
-const { request } = require('express');
 const Login = require('../models/loginModel');
 
 exports.index = (req, res) => {
-    res.render('login')
+    if (req.session.user) return res.render('login-logado')
+    return res.render('login')
 }
 
-exports.register = async (req, res) => {
-    const login = new Login(req.body);
-    await login.register();
+exports.register = async function (req, res) {
+    try {
+        const login = new Login(req.body);
+        await login.register();
 
-    if(login.errors.length > 0) {
-        req.flash('errors', login.errors);
-        req.session.save(function() {
-            res.redirect('back');
+        if (login.errors.length > 0) {
+            req.flash('errors', login.errors);
+            req.session.save(function () {
+                return res.redirect('/login/index');
+            })
+            return;
+        }
+        req.flash('success', 'Seu usuário foi criado com sucesso.');
+        req.session.save(function () {
+            return res.redirect('/login/index');
         })
+    } catch (e) {
+        console.log(e);
+        return res.render('404');
     }
 
-    res.send(login.errors);
+
+}
+
+exports.login = async function (req, res) {
+    try {
+        const login = new Login(req.body);
+        await login.login();
+
+        if (login.errors.length > 0) {
+            req.flash('errors', login.errors);
+            req.session.save(function () {
+                return res.redirect('/login/index');
+            })
+            return;
+        }
+
+        req.flash('success', 'Login feito com sucesso!');
+        req.session.user = login.user;
+        req.session.save(function () {
+            return res.redirect('/login/index');
+        })
+    } catch (e) {
+        console.log(e);
+        return res.render('404');
+    }
+
+
+}
+
+exports.logout = function (req, res) {
+    req.session.destroy();
+    res.redirect('/')
 }
