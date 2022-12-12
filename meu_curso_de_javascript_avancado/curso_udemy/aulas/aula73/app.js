b92d59362b0e49225994cac2be8dfa10c2ba73a0
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-12 19:59:42 -0300

    Aula 73

--- meu_curso_de_javascript_avancado/curso_udemy/aulas/aula73/app.js
+++ meu_curso_de_javascript_avancado/curso_udemy/aulas/aula73/app.js
@@ -14,20 +14,16 @@ Object.defineProperty (define uma propiedade)
 
 */
 
-const produto = {nome: 'caneca', preco: 1.8}
+const produto = { nome: "caneca", preco: 1.8 };
 
 const outraCoisa = {
-    ...produto,
-    
-
-}
+  ...produto,
+};
 
 // object.assing
 
-Object.defineProperty(produto, 'nome', {
-    writable: false
-})
-console.log(Object.getOwnPropertyDescriptor(produto, 'nome'))
-console.log(produto)
-
-
+Object.defineProperty(produto, "nome", {
+  writable: false,
+});
+console.log(Object.getOwnPropertyDescriptor(produto, "nome"));
+console.log(produto);
