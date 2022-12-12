2d5a43f8a0dfe3ecb048e136313fc754b95bd971
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-12 20:00:19 -0300

    Aula 74

--- meu_curso_de_javascript_avancado/curso_udemy/aulas/aula74/app.js
+++ meu_curso_de_javascript_avancado/curso_udemy/aulas/aula74/app.js
@@ -1,12 +1,12 @@
 function Pessoa(nome, sobrenome) {
-    this.nome = nome;
-    this.sobrenome = sobrenome;
-    // this.nomeCompleto = () => 'ORIGINAL:' + this.nome + ' ' + this.sobrenome;
+  this.nome = nome;
+  this.sobrenome = sobrenome;
+  // this.nomeCompleto = () => 'ORIGINAL:' + this.nome + ' ' + this.sobrenome;
 }
 
 Pessoa.prototype.nomeCompleto = function () {
-    return   this.nomeCompleto = () =>  this.nome + ' ' + this.sobrenome;
-}
+  return (this.nomeCompleto = () => this.nome + " " + this.sobrenome);
+};
 // instância
-const pessoa1 = new Pessoa('leo', 'o')
-console.dir(pessoa1)
+const pessoa1 = new Pessoa("leo", "o");
+console.dir(pessoa1);
