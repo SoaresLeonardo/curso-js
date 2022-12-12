b2679971513aac8068f237f120fbe46476f6ae40
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-12 17:28:10 -0300

    Exercício com numbers

--- meu_curso_de_javascript_avancado/curso_udemy/aulas/aula11/js/index.js
+++ meu_curso_de_javascript_avancado/curso_udemy/aulas/aula11/js/index.js
@@ -1,10 +1,10 @@
-// // window.alert('teste');
-// // window.confirm('testando');
-// // window.prompt('testando testando');
+// window.alert('teste');
+// window.confirm('testando');
+// window.prompt('testando testando');
 
-// let numero1 = window.prompt('numero1');
-// let numero2 = window.prompt('numero2');
+let numero1 = window.prompt('numero1');
+let numero2 = window.prompt('numero2');
 
-// let n1eN2 = numero1 + numero2;
+let n1eN2 = numero1 + numero2;
 
-// console.log(n1eN2);
+console.log(n1eN2);
