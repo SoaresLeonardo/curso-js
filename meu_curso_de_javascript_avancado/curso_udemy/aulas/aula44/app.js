6d488970afc37e1ed105b6d78d2e828851dc7532
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-12 18:39:42 -0300

    Aula 44 - Tratando e lançando erros

--- meu_curso_de_javascript_avancado/curso_udemy/aulas/aula44/app.js
+++ meu_curso_de_javascript_avancado/curso_udemy/aulas/aula44/app.js
@@ -4,18 +4,17 @@
 //     console.log('NaoExisto não existe')
 // }
 
-
 function soma(x, y) {
-    if(typeof x !== 'number' || typeof y !== 'number') {
-        throw new ReferenceError('x ou y precisam ser números!');
-    }
+  if (typeof x !== "number" || typeof y !== "number") {
+    throw new ReferenceError("x ou y precisam ser números!");
+  }
 
-    return x + y;
+  return x + y;
 }
 
 try {
-    console.log(soma(3 , 3))
-    console.log(soma('3' , 3))
-}catch(err) {
-    console.log(err)
+  console.log(soma(3, 3));
+  console.log(soma("3", 3));
+} catch (err) {
+  console.log(err);
 }
