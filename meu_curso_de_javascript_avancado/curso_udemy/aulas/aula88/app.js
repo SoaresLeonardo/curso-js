602e34f90a56fc0456d8a3c57348b38a95fbfba1
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-12 20:16:17 -0300

    Aula 88

--- meu_curso_de_javascript_avancado/curso_udemy/aulas/aula88/app.js
+++ meu_curso_de_javascript_avancado/curso_udemy/aulas/aula88/app.js
@@ -1,23 +1,21 @@
 function rand(min = 1, max = 5) {
-    min *= 1000
-    max *= 1000
-    return Math.floor(Math.random() * (max - min) + min);
+  min *= 1000;
+  max *= 1000;
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
 function esperaAi(msg, tempo) {
-    return new Promise((resolve, reject) => {   
-        setTimeout(() => {
-            if(typeof msg !== 'string') {
-                reject('Cai no erro')
-                return
-            }  
-            
-            resolve(msg.toUpperCase() + '- Passei na promise');
-        }, tempo)
-    });
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (typeof msg !== "string") {
+        reject("Cai no erro");
+        return;
+      }
 
+      resolve(msg.toUpperCase() + "- Passei na promise");
+    }, tempo);
+  });
 }
-
 
 // esperaAi('Fase 1' , rand())
 //     .then(valor => {
@@ -29,18 +27,17 @@ function esperaAi(msg, tempo) {
 //         return esperaAi('Fase 3', rand())
 //     })
 
-
 async function execulta() {
- try {
-    const fase1 =  await esperaAi('Fase 1' ,rand())
-    console.log(fase1)
-   const fase2 =  await esperaAi('Fase 2' ,rand())
-   console.log(fase2)
-   const fase3 =  await esperaAi('Fase 3' ,rand())
-   console.log(fase3)
- } catch(e) {
-    console.log(e)
- }
+  try {
+    const fase1 = await esperaAi("Fase 1", rand());
+    console.log(fase1);
+    const fase2 = await esperaAi("Fase 2", rand());
+    console.log(fase2);
+    const fase3 = await esperaAi("Fase 3", rand());
+    console.log(fase3);
+  } catch (e) {
+    console.log(e);
+  }
 }
 
-execulta()
+execulta();
