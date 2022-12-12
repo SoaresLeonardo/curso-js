d06558aa0ee4a35895aa50fab7cb8c9df2889036
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-12 18:31:44 -0300

    Aula 40 - Break e continue

--- meu_curso_de_javascript_avancado/curso_udemy/aulas/aula40/app.js
+++ meu_curso_de_javascript_avancado/curso_udemy/aulas/aula40/app.js
@@ -1,17 +1,16 @@
-const  numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-for(let numero of numeros) {
+for (let numero of numeros) {
+  if (numero === 2) {
+    console.log("Pulei o número 2");
+    continue;
+  }
 
-    if(numero === 2) {
-        console.log('Pulei o número 2')
-        continue;
-    }
+  if (numero === 7) {
+    console.log("7 encontrado saindo...");
+    break;
+    // A partir de break nada mais será executado!
+  }
 
-    if(numero === 7) {
-        console.log('7 encontrado saindo...')
-        break;
-        // A partir de break nada mais será executado!
-    }
-
-    console.log(numero);
+  console.log(numero);
 }
