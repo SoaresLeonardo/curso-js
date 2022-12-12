05785a3876334646a9a82fd8135e6850f0fcfe3a
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-12 18:12:36 -0300

    Aula 24

--- meu_curso_de_javascript_avancado/curso_udemy/aulas/aula24/index.js
+++ meu_curso_de_javascript_avancado/curso_udemy/aulas/aula24/index.js
@@ -1,13 +1,12 @@
 const hora = 13;
 // Estou falando para a linguagem executar a ação caso a minha hora for de 0 até 12
 // If pode ser usado sozinho
-if(hora <= 0 && hora <= 11) {
-   console.log('Bom dia')
+if (hora <= 0 && hora <= 11) {
+  console.log("Bom dia");
 } else if (hora > 12 && hora <= 17) {
-   console.log('Boa tarde')
-} else if(hora >= 18 && hora <= 23) {
-   console.log('Boa noite')
+  console.log("Boa tarde");
+} else if (hora >= 18 && hora <= 23) {
+  console.log("Boa noite");
 } else {
-   console.log('Erro')
-} 
-
+  console.log("Erro");
+}
