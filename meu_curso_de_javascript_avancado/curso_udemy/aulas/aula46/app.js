f5fe284779bc976a9b4d0493329c05c25f0c375a
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-12 18:42:36 -0300

    Aula 46 - setInterval e setTimeout

--- meu_curso_de_javascript_avancado/curso_udemy/aulas/aula46/app.js
+++ meu_curso_de_javascript_avancado/curso_udemy/aulas/aula46/app.js
@@ -1,19 +1,17 @@
 function mostraHora() {
-    let data =  new Date();
+  let data = new Date();
 
-    return data.toLocaleTimeString('pt-BR');
+  return data.toLocaleTimeString("pt-BR");
 }
 
 // function funcaoDoInterval() {
 //     console.log(mostraHora());
 // }
 
-
-const timer = setInterval(function (){
-    console.log(mostraHora());
+const timer = setInterval(function () {
+  console.log(mostraHora());
 }, 1000);
 
-
 setTimeout(function () {
-    clearInterval(timer);
-}, 5000)
+  clearInterval(timer);
+}, 5000);
