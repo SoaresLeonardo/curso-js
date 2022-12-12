c7dbaa3d97da11f1ec988ffd8b34723e2fe6c470
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-12 19:19:28 -0300

    Aula 61

--- meu_curso_de_javascript_avancado/curso_udemy/aulas/aula61/app.js
+++ meu_curso_de_javascript_avancado/curso_udemy/aulas/aula61/app.js
@@ -1,47 +1,41 @@
 // Funções geradoras
 
 function* geradora1() {
-    // Código qualquer...
-    yield 'Valor 1';
-    // Código qualquer...
-    yield 'Valor 2';
-    // Código qualquer...
-    yield 'Valor 3';
-
+  // Código qualquer...
+  yield "Valor 1";
+  // Código qualquer...
+  yield "Valor 2";
+  // Código qualquer...
+  yield "Valor 3";
 }
 
-
 function* geradora2() {
-    let i = 0
+  let i = 0;
 
-    while(true) {
-        yield i;
-        i++;
-    }
+  while (true) {
+    yield i;
+    i++;
+  }
 }
 
 function* geradora3() {
-    yield 0;
-    yield 1;
-    yield 2;
+  yield 0;
+  yield 1;
+  yield 2;
 }
 
 function* geradora4() {
-    yield* geradora3();
-    yield 3;
-    yield 4;
-    yield 5;
+  yield* geradora3();
+  yield 3;
+  yield 4;
+  yield 5;
 }
 
 const g4 = geradora4();
 
-for(let valor of g4) {
-    console.log(valor);
+for (let valor of g4) {
+  console.log(valor);
 }
-
-
-
-
 
 // Pegando os valores da função geradora
 // for(let valor of g1) {
