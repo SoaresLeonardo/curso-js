07c7d74f63a99de4be96f4ff3ea66191ef748f12
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-12 19:51:44 -0300

    Aula 64

--- meu_curso_de_javascript_avancado/curso_udemy/aulas/aula64/app.js
+++ meu_curso_de_javascript_avancado/curso_udemy/aulas/aula64/app.js
@@ -1,6 +1,6 @@
-const num1 = [1, 2 , 3]
-const num2 = [4, 5, 6]
+const num1 = [1, 2, 3];
+const num2 = [4, 5, 6];
 // ... rest => ... spread
-const num3 = [...num1, ...num2]
+const num3 = [...num1, ...num2];
 
-console.log(num3)
+console.log(num3);
