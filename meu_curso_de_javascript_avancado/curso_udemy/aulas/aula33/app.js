523556050089b091fa116ff0fef136e46f10bcf8
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-12 18:26:40 -0300

    Aula 33

--- meu_curso_de_javascript_avancado/curso_udemy/aulas/aula33/app.js
+++ meu_curso_de_javascript_avancado/curso_udemy/aulas/aula33/app.js
@@ -1,14 +1,13 @@
-const pessoa =  {
-    nome: 'Leonardo', 
-    sobrenome: 'Henrique',
-    idade: 30,
-    endereco: {
-        rua: 'Av Brasil',
-        numero: 42
-    }
+const pessoa = {
+  nome: "Leonardo",
+  sobrenome: "Henrique",
+  idade: 30,
+  endereco: {
+    rua: "Av Brasil",
+    numero: 42,
+  },
 };
 
+const { nome, sobrenome, idade, endereco: e } = pessoa;
 
-const {nome, sobrenome, idade, endereco: e} = pessoa;
-
-console.log(nome, e)
+console.log(nome, e);
