2a1701df3681c8c446525ebda86df49353de4bb6
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-12 19:55:05 -0300

    Aula 68

--- meu_curso_de_javascript_avancado/curso_udemy/aulas/aula68/app.js
+++ meu_curso_de_javascript_avancado/curso_udemy/aulas/aula68/app.js
@@ -2,11 +2,11 @@
 // Filtrar pares
 // Dobrar os valores
 // Reduzir (somar tudo)
-const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
 
-                            // Filter
-const NumerosPares = numeros.filter(valor => valor % 2 === 0)
-    .map(valor => valor * 2)
-    .reduce((ac, valor) => ac + valor)
+const NumerosPares = numeros
+  .filter((valor) => valor % 2 === 0)
+  .map((valor) => valor * 2)
+  .reduce((ac, valor) => ac + valor);
 
-console.log(NumerosPares)
+console.log(NumerosPares);
