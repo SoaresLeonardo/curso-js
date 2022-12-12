6ff66b488cafff1da5b6c08a44b3cc90ac70afe1
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-12 20:24:48 -0300

    Aula 92

--- meu_curso_de_javascript_avancado/curso_udemy/aulas/aula92/main.js
+++ meu_curso_de_javascript_avancado/curso_udemy/aulas/aula92/main.js
@@ -5,11 +5,10 @@
 //     }
 // }
 
-
 function Pessoa(nome, sobrenome) {
-   this.nome = nome;
-   this.sobrenome = sobrenome;
+  this.nome = nome;
+  this.sobrenome = sobrenome;
 }
 
-const p1 = new Pessoa('leo', 'henrique')
-console.log(p1)
+const p1 = new Pessoa("leo", "henrique");
+console.log(p1);
