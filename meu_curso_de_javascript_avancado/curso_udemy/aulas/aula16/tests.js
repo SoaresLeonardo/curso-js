678cea899cb294b53f73ca7eabab2c64f1b02445
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-12 17:52:50 -0300

    Aula 16

--- meu_curso_de_javascript_avancado/curso_udemy/aulas/aula16/tests.js
+++
@@ -1,8 +0,0 @@
-// let array = ['pedro', 'luiz', 'gustavo','joao']; 
-
-// // console.log(array[0]); --> mostra o que está na posição tal do array;
-// array.push('teste');
-
-// console.log(array);
-
-
