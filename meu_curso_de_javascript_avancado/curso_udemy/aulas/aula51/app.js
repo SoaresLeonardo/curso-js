da25d20514e7784d9e7e2b8e71843fbbbc841a55
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-12 18:52:09 -0300

    Aula 51

--- meu_curso_de_javascript_avancado/curso_udemy/aulas/aula51/app.js
+++ meu_curso_de_javascript_avancado/curso_udemy/aulas/aula51/app.js
@@ -1,33 +1,29 @@
-// return 
-// retona um valor
-// Termina função
+function soma(a, b) {
+    const soma = a+ b;
+    return soma; 
+}
 
-// function soma(a, b) {
-//     const soma = a+ b;
-//     return soma; 
-// }
+function Criamultiplicador(multiplicador) {
+    return function(n)  {
+        return  n * multiplicador
+    }
+}
 
-// function Criamultiplicador(multiplicador) {
-//     return function(n)  {
-//         return  n * multiplicador
-//     }
-// }
+const duplicar = Criamultiplicador(2)
 
-// const duplicar = Criamultiplicador(2)
+console.log(duplicar(2))
 
-// console.log(duplicar(2))
-
-// function criaPessoa(nome, sobrenome){
-//     return {nome, sobrenome}
-// }
+function criaPessoa(nome, sobrenome){
+    return {nome, sobrenome}
+}
 
 
-// function falaFrase(comeco) {
-//     function falaResto(resto) {
-//         return resto
-//     }
-//     return falaResto;
-// }
+function falaFrase(comeco) {
+    function falaResto(resto) {
+        return resto
+    }
+    return falaResto;
+}
 
 function falaFrase(comeco) {
     function falaResto(resto) {
