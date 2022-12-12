9224a82e53103791b93af30ca5ff56097ecea238
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-12 19:16:47 -0300

    Aula 60

--- meu_curso_de_javascript_avancado/curso_udemy/aulas/aula60/app.js
+++ meu_curso_de_javascript_avancado/curso_udemy/aulas/aula60/app.js
@@ -1,9 +1,9 @@
 function recursiva(max) {
-                    // se o número maximo for atingido paro minha função aqui;
-    if(max > 10) return;
-    max++
-    console.log(max)
-    recursiva(max)
+  // se o número maximo for atingido paro minha função aqui;
+  if (max > 10) return;
+  max++;
+  console.log(max);
+  recursiva(max);
 }
 
 recursiva(0);
