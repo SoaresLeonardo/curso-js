86818f0c9dd1ec554a4b0aaa7857742dc326c6c9
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-12 20:21:06 -0300

    Aula 79

--- meu_curso_de_javascript_avancado/curso_udemy/aulas/aula79/app.js
+++ meu_curso_de_javascript_avancado/curso_udemy/aulas/aula79/app.js
@@ -1,32 +1,29 @@
-const falar =  {
-    falar() {
-        console.log(`${this.nome} está falando.`)
-    }
+const falar = {
+  falar() {
+    console.log(`${this.nome} está falando.`);
+  },
 };
 
 const comer = {
-    comer() {
-        console.log(`${this.nome} está comendo.`)
-    }
+  comer() {
+    console.log(`${this.nome} está comendo.`);
+  },
 };
 
-const beber =  {
-    beber() {
-        console.log(`${this.nome} está bebendo.`)
-    }
+const beber = {
+  beber() {
+    console.log(`${this.nome} está bebendo.`);
+  },
 };
-                        //Object.assign({}, beber, falar)
-const pessoaPrototype = {...falar};
-
+//Object.assign({}, beber, falar)
+const pessoaPrototype = { ...falar };
 
 function criaPessoa(nome, sobrenome) {
-  return Object.create(pessoaPrototype,  {
-    nome: {value: nome},
-    sobrenome: {value: sobrenome}
-  })
+  return Object.create(pessoaPrototype, {
+    nome: { value: nome },
+    sobrenome: { value: sobrenome },
+  });
 }
 
-
-
-const p1 = criaPessoa('Leonardo', 'Henrique');
+const p1 = criaPessoa("Leonardo", "Henrique");
 console.log(p1);
