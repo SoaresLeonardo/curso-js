080c376f71e3bf0348c4f881cd7503dd694855a1
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-12 18:27:22 -0300

    Aula 34

--- meu_curso_de_javascript_avancado/curso_udemy/aulas/aula34/app.js
+++ meu_curso_de_javascript_avancado/curso_udemy/aulas/aula34/app.js
@@ -1,6 +1,5 @@
-const frutas = ['morango', 'maça', 'uva', 'pera']
+const frutas = ["morango", "maça", "uva", "pera"];
 
-for(let i = 0; i < frutas.length; i++) {
-    console.log(`Indice ${i}`, frutas[i])
+for (let i = 0; i < frutas.length; i++) {
+  console.log(`Indice ${i}`, frutas[i]);
 }
-
