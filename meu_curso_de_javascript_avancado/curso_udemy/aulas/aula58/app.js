4d13bf5c3a5d238bc7bf228c65e5dba3a79b352a
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-12 19:08:08 -0300

    Aula 58

--- meu_curso_de_javascript_avancado/curso_udemy/aulas/aula58/app.js
+++ meu_curso_de_javascript_avancado/curso_udemy/aulas/aula58/app.js
@@ -2,22 +2,21 @@
 // Função fabrica -> obj
 // construtora -> Pessoa(new)
 
-
 function Pessoa(nome, sobrenome) {
-    // Privadas
-    const ID = 123456;
+  // Privadas
+  const ID = 123456;
 
-    // Atributos públicos
-    this.nome = nome
-    this.sobrenome = sobrenome
+  // Atributos públicos
+  this.nome = nome;
+  this.sobrenome = sobrenome;
 
-    this.metodo = function() {
-        console.log(this.nome + ': sou um metodo')
-    }
+  this.metodo = function () {
+    console.log(this.nome + ": sou um metodo");
+  };
 }
 
-            // Cria obj vazio
-const p1 = new Pessoa('Leonardo', 'Henrique')
+// Cria obj vazio
+const p1 = new Pessoa("Leonardo", "Henrique");
 
 p1.metodo();
-// Pessoa.nome = 
+// Pessoa.nome =
