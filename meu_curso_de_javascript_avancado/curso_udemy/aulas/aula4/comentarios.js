c1b541dc70cba884016a35249e8ce5fec35f132e
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-12 16:17:30 -0300

    Comentários

--- meu_curso_de_javascript_avancado/curso_udemy/aulas/aula4/comentarios.js
+++ meu_curso_de_javascript_avancado/curso_udemy/aulas/aula4/comentarios.js
@@ -1,9 +1,9 @@
 // E escrever um comentário
 
 console.log('Olá mundo!'); // Aqui tem outro comentário
-
 console.log('Linha 1');
 console.log('Linha 2');
+
 // Comentário
 console.log('Linha 3');
 
@@ -14,4 +14,3 @@ console.log('Linha 3');
 
 console.log('Linha 4');
 // Vai ser ignorado
-
