9267195dab7672a7e4bacbfe561f0d440b2892a8
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-12 18:19:36 -0300

    Aula 28

--- meu_curso_de_javascript_avancado/curso_udemy/aulas/aula28/tests.js
+++
@@ -1,8 +0,0 @@
-const nivelUSuario = 1000;
-const setnivel = nivelUSuario >=1000 ? 'Usuario VIP' : 'Usuário normal';
-console.log(setnivel);
-// if(nivelUSuario >= 1000) {
-//     console.log('Usuário VIP')
-// } else {
-//     console.log('Usuário normal')
-// }
