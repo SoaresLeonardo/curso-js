264b2b533ec02146d4e0ecbc52092f931c398f12
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-12 18:30:25 -0300

    Aula 38

--- meu_curso_de_javascript_avancado/curso_udemy/aulas/aula38/assets/js/main.js
+++ meu_curso_de_javascript_avancado/curso_udemy/aulas/aula38/assets/js/main.js
@@ -1,12 +1,11 @@
-const parafragos =  document.querySelector('.paragrafos');
+const parafragos = document.querySelector(".paragrafos");
 
-const ps = parafragos.querySelectorAll('p')
+const ps = parafragos.querySelectorAll("p");
 
 const estiloBody = getComputedStyle(document.body);
 const backgroundColorBody = estiloBody.backgroundColor;
-for(let p of ps) {
-     console.log(p)
-     p.style.color = 'white'
-     p.style.backgroundColor = backgroundColorBody;
-     
+for (let p of ps) {
+  console.log(p);
+  p.style.color = "white";
+  p.style.backgroundColor = backgroundColorBody;
 }
