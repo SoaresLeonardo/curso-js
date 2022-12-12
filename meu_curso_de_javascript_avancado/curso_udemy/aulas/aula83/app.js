c0a3b7f4f4f18195c9d20cc3e675d95b10a47f1e
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-12 20:13:07 -0300

    Aula 83

--- meu_curso_de_javascript_avancado/curso_udemy/aulas/aula83/app.js
+++ meu_curso_de_javascript_avancado/curso_udemy/aulas/aula83/app.js
@@ -1,28 +1,28 @@
-class ControleRemoto  {
-    constructor(tv) {
-        this.tv = tv;
-        this.volume = 0;
-    }
+class ControleRemoto {
+  constructor(tv) {
+    this.tv = tv;
+    this.volume = 0;
+  }
 
-    // Método de instância
+  // Método de instância
 
-    aumentarVolume() {
-        this.volume += 2
-    }
+  aumentarVolume() {
+    this.volume += 2;
+  }
 
-    diminuirVolume() {
-        this.volume -= 2
-    }
+  diminuirVolume() {
+    this.volume -= 2;
+  }
 
-    // Método estático
+  // Método estático
 
-     static trocarPilha() {
-        console.log('ok vou trocar');
-    }
+  static trocarPilha() {
+    console.log("ok vou trocar");
+  }
 }
 
-const controle = new ControleRemoto('LG');
+const controle = new ControleRemoto("LG");
 
-ControleRemoto.trocarPilha()
+ControleRemoto.trocarPilha();
 
-console.log(controle)
+console.log(controle);
--- meu_curso_de_javascript_avancado/curso_udemy/aulas/aula83/teste.js
+++ meu_curso_de_javascript_avancado/curso_udemy/aulas/aula83/teste.js
@@ -1,17 +1,16 @@
 class Movel {
-    constructor(nome, preco) {
-        this.nome = nome;
-        this.preco = preco;
-    }
+  constructor(nome, preco) {
+    this.nome = nome;
+    this.preco = preco;
+  }
 }
 
 class Armario extends Movel {
-   constructor(nome, preco, cor) {
-    super(nome,preco)
+  constructor(nome, preco, cor) {
+    super(nome, preco);
     this.cor = cor;
-   }
-
+  }
 }
 
-const armario = new Armario('Armario 4 portas', 600, 'preto');
-console.log(armario)
+const armario = new Armario("Armario 4 portas", 600, "preto");
+console.log(armario);
