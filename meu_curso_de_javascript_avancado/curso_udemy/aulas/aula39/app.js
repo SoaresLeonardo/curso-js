4cc17cca7f3887f78602e1166ed75f183cac5272
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-12 18:30:45 -0300

    Aula 39

--- meu_curso_de_javascript_avancado/curso_udemy/aulas/aula39/app.js
+++ meu_curso_de_javascript_avancado/curso_udemy/aulas/aula39/app.js
@@ -13,17 +13,14 @@
 // //     x++
 // // }
 
-
 function random(min, max) {
-    const r = Math.random() * (max - min) + min;
-    return Math.floor(r);
+  const r = Math.random() * (max - min) + min;
+  return Math.floor(r);
 }
 
-let rand = random(1, 50)
+let rand = random(1, 50);
 
-
-
-while(rand !== 10) {
-    rand = random(1, 50)
-    console.log(rand)
+while (rand !== 10) {
+  rand = random(1, 50);
+  console.log(rand);
 }
