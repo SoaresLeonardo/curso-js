fda4c79a0a48129c92da33fcb2793b8936c574e3
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-12 17:26:12 -0300

    Numbers

--- meu_curso_de_javascript_avancado/curso_udemy/aulas/aula10/index.js
+++ meu_curso_de_javascript_avancado/curso_udemy/aulas/aula10/index.js
@@ -1,19 +1,5 @@
-const nome = 'Leo'; // "" ou '' // String
-const nome1 = 'Leo'; // String
-const nome2 = 'Leo'; // String
-const num1 = 10; // Number]
-const num2 = 10.52; // Number
-let nomeAluno; // undefined = não aponta pra local nenhum na memória
-const sobrenomeAluno = null; // Nulo aponta pra local nenhum na memória
-const aprovado = false; // Boolean =  true, false (lógico)
-
-let a = 2;
-const b = a;
-console.log(a,b); // 2, 2
-
-a = 3;
-console.log(a,b);  3, 2
-
-
-// String, number, undefined, null, boolean, symbol
+let num1 = 5
+let num2 = 2
+let num3 = 10
+console.log((num1 + num2) * num3)
 
--- meu_curso_de_javascript_avancado/curso_udemy/aulas/aula10/tests.js
+++
@@ -1,5 +0,0 @@
-let num1 = 5
-let num2 = 2
-let num3 = 10
-console.log((num1 + num2) * num3)
-
