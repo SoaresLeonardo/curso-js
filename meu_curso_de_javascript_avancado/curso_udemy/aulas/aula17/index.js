1ed7fbd291713b0f7d4ddb843f5e2fd52395bf80
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-12 17:54:50 -0300

    Aula 17

--- meu_curso_de_javascript_avancado/curso_udemy/aulas/aula17/index.js
+++ meu_curso_de_javascript_avancado/curso_udemy/aulas/aula17/index.js
@@ -1,75 +1,65 @@
-// function saudacao() {
-//             console.log('Bom dia!');
-// }
+function saudacao() {
+  console.log("Bom dia!");
+}
 
-// saudacao();
+saudacao();
 
-// function teste(nome) {
-//     let mensagem =  console.log(`Bom dia ${nome}`)
-//     return mensagem;
-// }
+function teste(nome) {
+  let mensagem = console.log(`Bom dia ${nome}`);
+  return mensagem;
+}
 
+teste("Leonardo");
 
-// teste('Leonardo');
-
-// escopo
-// function  somar(numero1, numero2) {
-//     return console.log(numero1 + numero2);
-// }
+function somar(numero1, numero2) {
+  return console.log(numero1 + numero2);
+}
 // função dentro de uma variavel; função anonima;
-// const raiz = (n) => {
-//     return n ** 0.5;
-// };
+const raiz = (n) => {
+  return n ** 0.5;
+};
 
-// console.log(raiz(9));
+console.log(raiz(9));
 
+// Modo simplificado de uma função com poucos parametros simples;
+const raizeteste = (y) => y ** 0.5;
 
-// // Modo simplificado de uma função com poucos parametros simples;
-// const raizeteste = y => y ** 0.5
+const pessoal = {
+  nome: "Leonardo",
+  sobrenome: "Henrique",
+  idade: 15,
+};
 
+// console.log(pessoal);
 
-// const pessoal =  {
-//     nome: 'Leonardo',
-//     sobrenome: 'Henrique',
-//     idade: 15
-// };
+function criapessoa(nome, sobrenome, idade) {
+  return { nome, sobrenome, idade };
+}
 
-// // console.log(pessoal);
+const pessoa1 = criapessoa("Leo", "henrique", 15);
+console.log(pessoa1);
 
-// function criapessoa(nome, sobrenome, idade) {
-//     return {nome, sobrenome, idade}
-// }
+const pessoa = {
+  nome: "Leonardo",
+  sobrenome: "Henrique",
+  idade: 15,
 
+  fala() {
+    // console.log(`${this.nome} ${this.sobrenome} mandou oi...`)
+    console.log(`Minha idade é ${this.idade} `);
+  },
 
-// const pessoa1 = criapessoa('Leo', 'henrique', 15 );
-// console.log(pessoa1)
+  icrementaidade() {
+    this.idade++;
+  },
+};
 
+pessoa.fala();
+pessoa.icrementaidade(); // Estoy chamando o metodo  dentro de pessoa e modificar no this.idade++;
+pessoa.fala();
 
-// const pessoa = {
-//     nome: 'Leonardo',
-//     sobrenome: 'Henrique',
-//     idade: 15,
+function somar(x, y) {
+  return console.log(x + y);
+}
 
-//     fala() {
-//         // console.log(`${this.nome} ${this.sobrenome} mandou oi...`)
-//         console.log(`Minha idade é ${this.idade} `)
-        
-//     },
-
-//     icrementaidade() {
-//         this.idade++;
-    
-//     }
-// };
-
-// pessoa.fala();
-// pessoa.icrementaidade(); // Estoy chamando o metodo  dentro de pessoa e modificar no this.idade++;
-// pessoa.fala();
-
-// function somar(x, y) {
-//     return console.log(x + y);
-// }
-
-// somar(5, 5)
-
-
+somar(5, 5);
