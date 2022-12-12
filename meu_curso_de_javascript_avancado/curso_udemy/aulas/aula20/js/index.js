15433aaac07f2a7e80b9dd272a6883405b3c00da
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-12 18:01:46 -0300

    Aula 20 - Exercício

--- meu_curso_de_javascript_avancado/curso_udemy/aulas/aula20/js/index.js
+++ meu_curso_de_javascript_avancado/curso_udemy/aulas/aula20/js/index.js
@@ -1,37 +1,35 @@
 function meuEscopo() {
-    const form = document.querySelector('.form');
-    const resultado = document.querySelector('.resultado');
+  const form = document.querySelector(".form");
+  const resultado = document.querySelector(".resultado");
 
-//     form.onsubmit  = function (evento) {
-//         evento.preventDefault();
-//         console.log('ok')
+  //     form.onsubmit  = function (evento) {
+  //         evento.preventDefault();
+  //         console.log('ok')
 
-// };
+  // };
 
-const pessoas = []
+  const pessoas = [];
 
-function recebeEventoForm(evento) { 
+  function recebeEventoForm(evento) {
     evento.preventDefault();
-    const nome = form.querySelector('.nome')
-    const sobrenome = form.querySelector('.sobrenome')
-    const peso = form.querySelector('.peso')
-    const altura = form.querySelector('.altura')
+    const nome = form.querySelector(".nome");
+    const sobrenome = form.querySelector(".sobrenome");
+    const peso = form.querySelector(".peso");
+    const altura = form.querySelector(".altura");
 
     // console.log(nome, sobrenome, peso, altura)
 
     pessoas.push({
-        nome: nome.value,
-        sobrenome: sobrenome.value,
-        peso: peso.value,
-        altura: altura.value
-    })
+      nome: nome.value,
+      sobrenome: sobrenome.value,
+      peso: peso.value,
+      altura: altura.value,
+    });
 
-    console.log(pessoas)
-    resultado.innerHTML += `${nome.value} ${sobrenome.value} ${peso.value} ${altura.value} <br>`
-}
+    console.log(pessoas);
+    resultado.innerHTML += `${nome.value} ${sobrenome.value} ${peso.value} ${altura.value} <br>`;
+  }
 
- 
-
-form.addEventListener('submit', recebeEventoForm)
+  form.addEventListener("submit", recebeEventoForm);
 }
 meuEscopo();
