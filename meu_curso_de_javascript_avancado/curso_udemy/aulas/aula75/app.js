dba18b9976c7927a4908076993b9e89d49e69a00
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-12 20:18:28 -0300

    Aula 75

--- meu_curso_de_javascript_avancado/curso_udemy/aulas/aula75/app.js
+++ meu_curso_de_javascript_avancado/curso_udemy/aulas/aula75/app.js
@@ -15,28 +15,28 @@
 // console.log(objB)
 
 function Produto(nome, preco) {
-    this.nome = nome;
-    this.preco = preco;
+  this.nome = nome;
+  this.preco = preco;
 }
 
-Produto.prototype.desconto = function(percentual) {
-    this.preco = this.preco - (this.preco * (percentual / 100))
-}
+Produto.prototype.desconto = function (percentual) {
+  this.preco = this.preco - this.preco * (percentual / 100);
+};
 
-Produto.prototype.aumento = function(percentual) {
-    this.preco = this.preco + (this.preco * (percentual / 100))
-}
+Produto.prototype.aumento = function (percentual) {
+  this.preco = this.preco + this.preco * (percentual / 100);
+};
 
 const p2 = {
-    nome: 'Caneca',
-    preco: 15
-}
+  nome: "Caneca",
+  preco: 15,
+};
 
-Object.setPrototypeOf(p2, Produto.prototype)
+Object.setPrototypeOf(p2, Produto.prototype);
 
-p2.aumento(10)
+p2.aumento(10);
 
-const p1 = new Produto('Camiseta', 50)
-p1.aumento(100)
-console.log(p1)
-console.log(p2)
+const p1 = new Produto("Camiseta", 50);
+p1.aumento(100);
+console.log(p1);
+console.log(p2);
