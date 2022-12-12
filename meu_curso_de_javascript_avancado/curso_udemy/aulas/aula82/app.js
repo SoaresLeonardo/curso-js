93c6a92b5ba5f3d231919b5cae4e5722946fb72f
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-12 20:12:15 -0300

    Aula 82

--- meu_curso_de_javascript_avancado/curso_udemy/aulas/aula82/app.js
+++ meu_curso_de_javascript_avancado/curso_udemy/aulas/aula82/app.js
@@ -1,49 +1,48 @@
 class DispositivoEletronico {
-    constructor(nome) {
-        this.nome = nome;
-        this.ligado = false;
+  constructor(nome) {
+    this.nome = nome;
+    this.ligado = false;
+  }
+
+  ligar() {
+    if (this.ligado) {
+      console.log(`${this.nome} já ligado`);
+      return;
+    }
+    this.ligado = true;
+  }
+
+  desligar() {
+    if (!this.ligado) {
+      console.log(`${this.nome} já desligado`);
     }
 
-    ligar() {
-        if(this.ligado) {
-            console.log(`${this.nome} já ligado`)
-            return;
-        }
-        this.ligado = true
-    }
-
-    
-    desligar() {
-        if(!this.ligado) {
-            console.log(`${this.nome} já desligado`)
-        }
-
-        this.ligado = false
-    }
+    this.ligado = false;
+  }
 }
 
 class Smartphone extends DispositivoEletronico {
-    constructor(nome, cor) {
-        super(nome)
-        this.cor = cor;
-    }   
+  constructor(nome, cor) {
+    super(nome);
+    this.cor = cor;
+  }
 }
 
 class Tablet extends DispositivoEletronico {
-    constructor(nome, temWifi) {
-        super(nome);
-        this.temWifi = temWifi;
-    }
-    
-    ligar() {
-        console.log('Olá')
-    }
+  constructor(nome, temWifi) {
+    super(nome);
+    this.temWifi = temWifi;
+  }
+
+  ligar() {
+    console.log("Olá");
+  }
 }
 
-const t1 = new Tablet('ipad', true);
+const t1 = new Tablet("ipad", true);
 
-const s1 =  new Smartphone('iphone','Preto')
-s1.ligar()
-console.log(s1)
-t1.ligar()
-console.log(t1)
+const s1 = new Smartphone("iphone", "Preto");
+s1.ligar();
+console.log(s1);
+t1.ligar();
+console.log(t1);
