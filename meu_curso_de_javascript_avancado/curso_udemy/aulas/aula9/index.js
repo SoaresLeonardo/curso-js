80fdc3fc9a69dcdbd3082ae341c4773c9a1cd2fb
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-12 17:23:56 -0300

    Tipo de dados

--- meu_curso_de_javascript_avancado/curso_udemy/aulas/aula9/index.js
+++ meu_curso_de_javascript_avancado/curso_udemy/aulas/aula9/index.js
@@ -1,6 +1,19 @@
-// O var caso ouver uma tentativa de declarar ela novamente o seu valor será alterado coisa que let não permitiria.
+const nome = 'Leo'; // "" ou '' // String
+const nome1 = 'Leo'; // String
+const nome2 = 'Leo'; // String
+const num1 = 10; // Number]
+const num2 = 10.52; // Number
+let nomeAluno; // undefined = não aponta pra local nenhum na memória
+const sobrenomeAluno = null; // Nulo aponta pra local nenhum na memória
+const aprovado = false; // Boolean =  true, false (lógico)
 
-var nome = 'Leonardo';
-var nome = 'Henrique';
-variavelSemDeclaracao = 'Teste' // NÃO FAZER ISSO!!
-console.log(nome);
+let a = 2;
+const b = a;
+console.log(a,b); // 2, 2
+
+a = 3;
+console.log(a,b);  3, 2
+
+
+// String, number, undefined, null, boolean, symbol
+
--- meu_curso_de_javascript_avancado/curso_udemy/aulas/aula9/tests.js
+++
@@ -1,6 +0,0 @@
-let nome = 'Leonardo';
-//let nome = 'Leonardo';
-var y = 'Teste';
-var y = 'Outro teste';
-console.log(nome);
-console.log(y)
