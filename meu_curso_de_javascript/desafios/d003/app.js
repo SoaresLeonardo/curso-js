fbd20dd7b6780f307fe7060385ad6f4db1cdad7a
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-12 20:32:06 -0300

    Update

--- meu_curso_de_javascript/desafios/d003/app.js
+++
@@ -1,7 +0,0 @@
-function antcs() {
-    var numero =  prompt('Digite um número inteiro qualquer')
-    var x = numero -1
-    var y = x +2
-    window.alert(`Antes do ${numero}, Temos o número ${x}.
-Depois de ${numero}, temos ${y}.`)
-}
