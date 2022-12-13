35b8df8ed72d5fa8dbe27cb33a3a1ef185f80077
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-13 14:07:18 -0300

    Aula 9

--- aula9/index.js
+++ aula9/index.js
@@ -1,3 +1,5 @@
+// String, number, undefined, null, boolean, symbol
+
 const nome = 'Leo'; // "" ou '' // String
 const nome1 = 'Leo'; // String
 const nome2 = 'Leo'; // String
@@ -7,13 +9,14 @@ let nomeAluno; // undefined = não aponta pra local nenhum na memória
 const sobrenomeAluno = null; // Nulo aponta pra local nenhum na memória
 const aprovado = false; // Boolean =  true, false (lógico)
 
+
 let a = 2;
 const b = a;
 console.log(a,b); // 2, 2
+
 
 a = 3;
 console.log(a,b);  3, 2
 
 
-// String, number, undefined, null, boolean, symbol
 
