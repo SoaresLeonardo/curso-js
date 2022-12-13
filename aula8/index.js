a59c033a44006508e4c02af3176968e003189858
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-13 13:46:12 -0300

    Calculando IMC (Básico)

--- aula8/index.js
+++ aula8/index.js
@@ -1,8 +1,3 @@
-/*
-    Luiz Otávio Miranda tem 30 anos, pesa 84 kg
-    tem 1.8 de altura e seu IMC é de 25.925925925925924
-*/
-
 const nome = 'Leonardo';
 const sobrenome = 'Henrique';
 const idade = '15';
@@ -10,7 +5,6 @@ const peso = '73';
 const altura = '1.79';
 let imc; // peso / (altura * altura)
 let anoNascimento; // ano de nascimento
-
 imc = peso / (altura * altura)
 anoNascimento = 2022 - idade
 console.log(nome, sobrenome, 'tem', idade, 'pesa', peso, 'kg');
