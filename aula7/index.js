eb4cfd0d63527c05ad274cc91bc8249a6d6398e3
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-13 13:25:11 -0300

    Constantes

--- aula7/index.js
+++ aula7/index.js
@@ -1,6 +1,6 @@
 // Não podemos criar variáveis com palavras reservadas
 // Variáveis precisam ter nomes significativos
 // Não da para alterar o valor de uma const
-const nome = 'João';
+const nome = 'Leonardo';
 console.log(nome);
 
