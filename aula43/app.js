afcdd55845ac5d1c81a514d1067c148da983551c
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-13 12:34:42 -0300

    Aula 43 - Lógica de programação - 03

--- aula43/app.js
+++ aula43/app.js
@@ -9,6 +9,7 @@ function FizzBuzz(numero) {
   if (numero % 3 === 0 && numero % 5 === 0) return "FizzBuzz";
   if (numero % 3 === 0) return "Fizz";
   if (numero % 5 === 0) return "Buzz";
+  
   return numero;
 }
 
